669b760a21dfb6c16fe115338213a0ccafd3369f
+ <+>
2023-03-02 11:05:57 +0300

    models complete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,3 +2,13 @@ class Blog < ActiveRecord::Base
     has_many :reviews
     has_many :readers, through: :reviews
 end
+
+# In this example, we're adding a has_many :readers, through: :reviews association to the Blog
+# model. This allows us to retrieve all the Readers who have written Reviews for a 
+# particular Blog using the readers association.
+
+# We can also use this association to create new Reviews for a Blog with a specific Reader. 
+
+# blog = Blog.find(1)
+# reader = Reader.find(1)
+# blog.reviews.create(reader: reader, content: "Great post!", rating: 5)
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,7 @@
 class Reader < ActiveRecord::Base
    has_many :reviews
+   has_many :blogs, through: :reviews
 end
+
+# You can also retrieve all the Blogs that a Reader has written Reviews for using
+# a similar has_many :through association in the Reader model:
